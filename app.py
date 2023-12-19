22c25a10f584c4870cb54582ddd804114bc9e34c
mateoguevaraa <mateoaguevara@gmail.com>
2023-12-19 19:45:29 -0300

    Falta solucionar tabla sessions para que guarde la información

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-import os
-
 from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sessions.db'
-app.config['SESSION_TYPE'] = 'sqlalchemy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app_data.db' # Database for app data (default db)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
 
-app.config['SESSION_SQLALCHEMY'] = db
 
-sess = Session(app)
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class SessionData(db.Model):
+    __tablename__ = 'sessions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.String(100), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    user = db.relationship("User", backref="sessions")
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    task_name = db.Column(db.String(100), nullable=False)
+    user = db.relationship('User', backref='tasks')  # Establish a relationship with the User table
+
+with app.app_context():
+    db.create_all()
+
+
+
 
 @app.route("/")
 def inicio():
     return 'ok'
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
