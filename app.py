8941b46f2276770f79876fda95e0f80d632a9dd3
mateoguevaraa <mateoaguevara@gmail.com>
2023-12-19 21:27:26 -0300

    ya estan las base de datos listas

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Configure Flask-Session to use SQLAlchemy for session storage
+app.config['SESSION_TYPE'] = 'sqlalchemy'
+app.config['SESSION_SQLALCHEMY'] = db
+app.config['SESSION_SQLALCHEMY_TABLE'] = 'sessions'
+
+# Initialize Flask-Session
+Session(app)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -17,15 +25,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-class SessionData(db.Model):
-    __tablename__ = 'sessions'
-
-    id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.String(100), unique=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    user = db.relationship("User", backref="sessions")
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -37,7 +36,6 @@ class Task(db.Model):
 
 with app.app_context():
     db.create_all()
-
 
 
 
